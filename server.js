d542f5fbb0f21c47c3af015887be6587b6bc6405
Claudio Costa <costa.pederneschi@gmail.com>
2026-01-30 15:56:08 +0000

    adiciona middleware CORS para permitir requisições de origens específicas

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
+// Middleware CORS para liberar todas as origens
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://binancesocket.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Endpoint para obter o saldo atual (USD, BTC, posições e último preço do BTC)
 app.get('/saldo', (req, res) => {
